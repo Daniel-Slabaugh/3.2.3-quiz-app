c1ad7171136a8feaeaddc5def605deeba82e10fb
Daniel-Slabaugh <danielslabaugh@gmail.com>
2017-01-11 18:36:36 -0600

    add some code

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ var state = {
 
 
 //State modification 
-var checkQuestion = function() {
+var correctAnswer = function(state) {
+	state.correct += 1;
+};	
 
+var incorrectAnswer = function(state) {
+	state.incorrect += 1;
+};	
 
-};
 
 var nextQuestion = function() {
 
 };
 
 var startQuiz = function(state, event) {
-	    $(event.currentTarget).addClass('hidden');
-	    var questions = $('.quiz').children();
-	    $(questions[0]).removeClass('hidden');
 	    state.currentQuestion = 1;
 };
 
@@ -30,14 +31,19 @@ var renderQuestion = function(state) {
 
 
 
-
 //Event listeners
 $('#intro').submit(function(event) {
-	    event.preventDefault();
+		event.preventDefault();
+		$(event.currentTarget).addClass('hidden');
+	    var questions = $('.quiz').children();
 	    startQuiz(state, event);
 });
 
 $('.quiz-question').on('click', '.choice', function(event) {
 	event.preventDefault();
-	checkQuestion(state, event);
+	if($(event.currentTarget).hasClass('correct')) {
+		correctAnswer(state);
+	} else {
+		incorrectAnswer(state);
+	}
 });
